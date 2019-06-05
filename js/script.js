d8771cd3e94ae83a6d6cc00766f7985ddc2f0946
Paweł Żurawka <pzurawka@gmail.com>
2019-06-05 18:31:32 +0200

    Fix variables

--- js/script.js
+++ js/script.js
@@ -9,10 +9,10 @@ getJoke();
 const paragraph = document.getElementById('joke');
 
 function getJoke() {
-    let xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
     xhr.open('GET', url);
     xhr.addEventListener('load', function() {
-        let response = JSON.parse(xhr.response);
+        const response = JSON.parse(xhr.response);
         paragraph.innerHTML = response.value.joke;
     });
     xhr.send();
